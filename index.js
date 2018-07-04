bb11437e438072d0915cc60953f2ee77c3e9fc96
Sebas <seplarui@gmail.com>
2018-07-04 20:25:18 +0200

    Acceso mysql

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var express = require('express');
-var app = expres();
+var app = express();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    database: 'GestionTareas',
+    host: 'localhost',
+    user: 'root',
+    password: ''
+});
+
 
 app.get('/', function(req, res) {
-    res.send("Hola Mundo");
+
+    // connection.connect();
+    connection.query('select * from TAREASESTADOS', function(err, rows, fields) {
+            if (err) throw err;
+            //console.log('El resultado es: ', rows[0]);
+            res.send(rows);
+        })
+        //connection.end();
+        //res.send("Hola Mundo");
 });
 
 app.listen(3000, function() {
